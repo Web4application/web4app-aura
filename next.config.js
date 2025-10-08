3f987d16febfb3cedd2bfdbd8f968ba15110a927
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-08 01:44:47 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/Web4app',
+  assetPrefix: '/Web4app/'
+};
+module.exports = nextConfig;
